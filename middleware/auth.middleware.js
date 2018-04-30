d86e00d568af028a627dafc1e581b8e17f3536e2
codeekage <agiriabrahamjunior@gmail.com>
2018-04-30 12:20:47 +0100

    added comments

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//AUTHENTICATION MIDDLEWARE
+//AUTHENTICATION MIDDLEWARE FOR JWT
 /**
  * ENFORCE AUTHENTICATION ON ROUTES
  * @param {client request} req 
@@ -8,15 +8,21 @@
  * @param {next function} next 
  */
 const ensureAuth = (req, res, next) => {
+    //GET AUTHRORICATION FROM HEADER ARRAY
     const bearerHeader = req.headers['authorization'];
+    //CHECK IF HEADER IS UNDEFIEND
     if (typeof bearerHeader != 'undefined') {
+        //GET TOKEN FROM FROM BEARER VALUE [AUTHORIZATION : BEARER <TOKEN>]
         const bearerToken = bearerHeader.split(' ')[1];
+        //DEFINE NEW GLOBAL VARIBABLE [req.token];
         req.token = bearerToken;
+        //PROCEED WITH THREAD
         next();
     } else {
+        //FAILED
         res.sendStatus(403);
     }
 }
 
-
+//MODULE EXPORT
 module.exports = ensureAuth;
